d7691b330f71898197fe0501895193f8bc06df02
Alexandr Romanishin <alexandr.romanishin@tbwamobile.com>
2019-07-07 15:52:06 +0300

    Added Horizontal PageView with keepeing state for each page. Added scroll to QuoteWidget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'dart:ui'
+import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -10,54 +10,49 @@ import 'package:simpons_quotes/ImageRotationWidget.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  Map<int, Widget> pagesHolder = Map();
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     const title = 'Simpons Quotes';
     var textColor = TextStyle(color: Colors.yellow);
-    final titleTheme = Theme
-        .of(context)
-        .textTheme
-        .title
-        .merge(textColor);
-    final display1 = Theme
-        .of(context)
-        .textTheme
-        .display1
-        .merge(textColor);
-    final display3 = Theme
-        .of(context)
-        .textTheme
-        .display3
-        .merge(textColor);
+    final titleTheme = Theme.of(context).textTheme.title.merge(textColor);
+    final display1 = Theme.of(context).textTheme.display1.merge(textColor);
+    final display3 = Theme.of(context).textTheme.display3.merge(textColor);
     return MaterialApp(
-      title: title,
-      theme: ThemeData(
-        fontFamily: "simpsonfont",
-        textTheme: TextTheme(
-            title: titleTheme, display3: display3, display1: display1),
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
+        title: title,
+        theme: ThemeData(
+          fontFamily: "simpsonfont",
+          textTheme: TextTheme(
+              title: titleTheme, display3: display3, display1: display1),
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
           appBar: AppBar(title: Text(title)),
-          body:
-          Container(
+          body: Container(
               alignment: Alignment.center,
               decoration: BoxDecoration(
                   image: DecorationImage(
                       image: AssetImage("resources/background.jpg"),
                       fit: BoxFit.cover)),
-              child: MainPage())),
-    );
+              child: PageView.builder(
+                  itemBuilder: (context, index) => MainPage(index))),
+        ));
   }
 }
 
 class MainPage extends StatefulWidget {
+  int index;
+
+  MainPage(this.index) : super(key: Key("$index"));
+
   @override
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage>
+    with AutomaticKeepAliveClientMixin<MainPage> {
   Future<Quote> _loadDataFuture;
 
   @override
@@ -67,17 +62,19 @@ class _MainPageState extends State<MainPage> {
   }
 
   @override
+  bool get wantKeepAlive => true;
+
+  @override
   Widget build(BuildContext context) {
     return FutureBuilder(
         future: _loadDataFuture,
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.waiting:
-              return new CircularProgressIndicator();
+              return DonutLoader();
             case ConnectionState.done:
               if (snapshot.hasData) {
-                return DonutLoader();
-//                return new QuoteWidget(quote: snapshot.data);
+                return new QuoteWidget(quote: snapshot.data);
               } else {
                 return new Text(
                   '${snapshot.error}',
@@ -114,23 +111,24 @@ class QuoteWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var textStyle = Theme
-        .of(context)
-        .textTheme
-        .display1
-        .merge(TextStyle(
-      backgroundColor: Color(0x9A42A5F5),
-    ));
-    return Stack(
-//      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Image.network(quote.image),
-        Text(
-          quote.quote,
-          textAlign: TextAlign.center,
-          style: textStyle,
-        )
-      ],
+    var textStyle = Theme.of(context).textTheme.display1.merge(TextStyle(
+          backgroundColor: Color(0x9A42A5F5),
+        ));
+    return SingleChildScrollView(
+      padding: EdgeInsets.all(16.0),
+      physics: AlwaysScrollableScrollPhysics(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            quote.quote,
+            textAlign: TextAlign.center,
+            style: textStyle,
+          ),
+          SizedBox(height: 32.0),
+          Image.network(quote.image),
+        ],
+      ),
     );
   }
 }
@@ -169,4 +167,3 @@ class DonutLoader extends StatelessWidget {
   Widget build(BuildContext context) =>
       ImageRotationWidget(Image.asset("resources/donutLoader.png"));
 }
-
